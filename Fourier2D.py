c1543f2b39ad7e98bcb13b15ca358fe07d32a547
Camila  Garcia C <camilagarciac@192.168.0.5>
2018-10-28 17:20:14 -0500

    Actualizacion fourier 2D

---
+++ Fourier2D.py
@@ -0,0 +1,8 @@
+import matplotlib.pylab as plt
+import numpy as np
+import scipy as sp
+from scipy.fftpack import fft, fftfreq, ifft, fft2, ifft2
+from scipy import misc
+from skimage import io
+
+arbol=plt.imread("arbol.png")
