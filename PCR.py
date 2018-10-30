284bccd047fe059c421d7615a9198c393cca9839
Camila  Garcia C <camilagarciac@192.168.0.5>
2018-10-29 21:35:55 -0500

    se declaro la matriz

--- PCR.py
+++ PCR.py
@@ -1,11 +1,12 @@
 #Se importa el paquete de numpy y matplotlib
-
 import numpy as np 
 import matplotlib.pyplot as plt 
 from numpy.linalg import *
 
+#Se cargan los datos y se especifica que datos usar
 data = np.genfromtxt("WDBC.dat",delimiter=",", usecols=(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31))
 
+#Se declara la matriz de covarianza
 def covarianza(x): 
 	size = np.shape(x)[1]
 	p = np.shape(x)[0] 
