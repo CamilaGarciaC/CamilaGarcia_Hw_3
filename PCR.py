8dfd462043cc2fe3c899f90ff3f5a6e886c3f1f4
Camila  Garcia C <camilagarciac@192.168.0.5>
2018-10-29 21:32:31 -0500

    se declaro la matriz

--- PCR.py
+++ PCR.py
@@ -2,5 +2,21 @@
 
 import numpy as np 
 import matplotlib.pyplot as plt 
+from numpy.linalg import *
 
-sdfksdfksdfsadfsdf
+data = np.genfromtxt("WDBC.dat",delimiter=",", usecols=(2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31))
+
+def covarianza(x): 
+	size = np.shape(x)[1]
+	p = np.shape(x)[0] 
+	cov = np.zeros([d,d]) 
+	for i in range(size):
+	    for j in range(size):
+	        meani = np.mean(x[:,i])
+		meanj = np.mean(x[:,j])
+		cov[i,j] = (np.sum((x[:,i]-meani)*(x[:,j]-meanj)))/(p-1)
+	return cov 
+	
+matriz = covarianza(data)
+
+
