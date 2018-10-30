c024772ec2bcaad71f7bb0736aa8d9a8dc6107c4
Camila  Garcia C <camilagarciac@MacBook-Air-de-Camila-6.local>
2018-10-30 17:17:37 -0500

    Gráfica de fourier 2D

--- Fourier2D.py
+++ Fourier2D.py
@@ -1,8 +1,19 @@
+#Se importan los paquetes
 import matplotlib.pylab as plt
 import numpy as np
 import scipy as sp
 from scipy.fftpack import fft, fftfreq, ifft, fft2, ifft2
 from scipy import misc
 from skimage import io
+from matplotlib.colors import LogNorm
 
-arbol=plt.imread("arbol.png")
+#Se almacenan los datos en un arreglo nupy
+data =plt.imread("arbol.png")
+arr=np.array(data)
+
+imagen1=fft2(arr)
+imagen2=imagen1.copy()	
+
+plt.figure()
+plt.plot(abs(imagen1), norm=LogNorm(vmin=1))
+plt.savefig("GarciaCamila_FT2D.pdf")
