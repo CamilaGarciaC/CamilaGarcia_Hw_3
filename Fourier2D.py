0b88ff246d264c1b427bd8e970b9ce9d748bf6e0
Camila  Garcia C <camilagarciac@MacBook-Air-de-Camila-6.local>
2018-10-31 17:47:00 -0500

    Fourier 2D casi completo

--- Fourier2D.py
+++ Fourier2D.py
@@ -2,13 +2,13 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import math 
+from matplotlib.colors import LogNorm
 from scipy.fftpack import fft2, fftshift, ifftshift, ifft2
 from scipy.signal import convolve2d
 
 #Se alamacena la imagen en un arreglo de numpy
 img=plt.imread('arbol.png')
-x, y = img.shape 
-
+ 
 #Usando los paquetes, se hace la transformada de la imagen
 transf=fft2(img)
 transf_s=fftshift(transf)
@@ -18,8 +18,32 @@ transf_s_abs=abs(transf_s)
 log_transf=np.log(transf_s_abs)
 
 #Se grafica la transformada y se guarda
-plt.imshow(log_transf)
+plt.imshow(log_transf,plt.cm.gray)
 plt.title("Transformada de Fourier")
 plt.xlabel("Frecuencia")
 plt.ylabel("y(t)")
 plt.savefig("GarciaCamila_FT2D.pdf")
+
+def filtrar(transf):
+    for i in range(len(transf)):
+        for j in range(len(transf)):
+            if (abs(transf[i,j])>4000.0 and abs(transf[i,j])<5000.0 ):
+                transf[i,j]=0
+            else:
+                transf[i,j]=transf[i,j]
+    return transf
+
+filtrarTransf=filtrar(transf_s)
+
+plt.figure()
+plt.imshow(np.abs(filtrarTransf),norm=LogNorm(vmin=5),cmap='gray')
+plt.title('Transformada de Fourier usando filtro')
+plt.savefig("GarciaCamila_FT2D_Filtrada.pdf")
+
+arbolfinal=np.real(ifft2(ifftshift(filtrarTransf)))
+
+plt.figure()
+plt.title('Transformada inversa de Fourier filtrada')
+plt.imshow(np.abs(arbolfinal),plt.cm.gray)
+plt.savefig("GarciaCamila_Imagen_filtrada.pdf")
+
