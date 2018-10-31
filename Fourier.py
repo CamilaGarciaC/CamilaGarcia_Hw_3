53f676a02f45831ee2c655a8767f329870f6d090
Camila  Garcia C <camilagarciac@192.168.0.5>
2018-10-30 23:29:58 -0500

    Adelanto fourier

--- Fourier.py
+++ Fourier.py
@@ -7,18 +7,37 @@ from scipy import interpolate
 
 #Se carga y se almacena los datos de signal.dat
 signal = np.genfromtxt("signal.dat",delimiter=",")
-xsig=signal[:,0]
-ysig=signal[:,1]
+x_sig=signal[:,0]
+y_sig=signal[:,1]
+n=len(signal)
+nx=len(x_sig)
 
 #Se carga y almacena los datos de incompletos.dat
 incompletos = np.genfromtxt("incompletos.dat",delimiter=",")
-xinc=incompletos[:,0]
-yinc=incompletos[:,1]
+x_inc=incompletos[:,0]
+y_inc=incompletos[:,1]
 
 #Grafica de datos de signal.dat y se guarda sin mostrarla 
 plt.figure()
-plt.plot(xsig,ysig,label="Signal")
-plt.xlabel("Tiempo")
-plt.ylabel("y(x)")
+plt.plot(x_sig,y_sig,label="Signal")
+plt.xlabel("x")
+plt.ylabel("y")
 plt.title("Signal")
 plt.savefig("GarciaCamila_signal.pdf")
+
+
+#Se declara el sample spacing y rating
+spacing = xsen[1]-xsen[0]
+rating = 1/spacing
+
+#Se define la funcion de transformada
+def T(x_sig,y_sig):
+	contador=np.linspace(0,0, nx)
+	for i in range(nx):
+		for j in range(nx):
+			contador[i]=contador[i]+(np.exp(-1j*2*np.pi*j*i/nx)*y_sig[j])
+	contador=contador/nx
+	return contador
+contador=abs(T(x_sig,y_sig))
+
+
