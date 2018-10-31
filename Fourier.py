2c7cd2464b1d07f4ea49f1074fc7c45f5ba8c75e
CamilaGarciaC <42305253+CamilaGarciaC@users.noreply.github.com>
2018-10-30 23:32:39 -0500

    Update Fourier.py

--- Fourier.py
+++ Fourier.py
@@ -9,8 +9,6 @@ from scipy import interpolate
 signal = np.genfromtxt("signal.dat",delimiter=",")
 x_sig=signal[:,0]
 y_sig=signal[:,1]
-n=len(signal)
-nx=len(x_sig)
 
 #Se carga y almacena los datos de incompletos.dat
 incompletos = np.genfromtxt("incompletos.dat",delimiter=",")
@@ -25,19 +23,32 @@ plt.ylabel("y")
 plt.title("Signal")
 plt.savefig("GarciaCamila_signal.pdf")
 
+#Se hace la definicion de transformada de fourierpara los datos de la señal.
+def DFT(x):
+    N = len(x)
+    n= np.arange(len(x))
+    F_k=[]
+    for k in range(N):
+        omega = 2*np.pi*n*k/N
+        F_real = x*np.cos(2*np.pi*n*k/N)
+        F_im = x*np.sin(2*np.pi*n*k/N)
+        F_r = np.sum(F_real)
+        F_i = np.sum(F_im)
+        Fk= F_r -1j*F_i
+        F_k.append(Fk)
+    return F_k
 
-#Se declara el sample spacing y rating
-spacing = xsen[1]-xsen[0]
-rating = 1/spacing
+#Se declara la función de las frecuencias utilizando el paquete fftfreq
+n_sig=len(x_sig)
+dx_sig=x_sig[1]-x_sig[0]
+f_sig=fftfreq(n_sig, dx_sig)
+print("Se uso el paquete de fftfreq para la grafica de transformada")
 
-#Se define la funcion de transformada
-def T(x_sig,y_sig):
-	contador=np.linspace(0,0, nx)
-	for i in range(nx):
-		for j in range(nx):
-			contador[i]=contador[i]+(np.exp(-1j*2*np.pi*j*i/nx)*y_sig[j])
-	contador=contador/nx
-	return contador
-contador=abs(T(x_sig,y_sig))
-
-
+#Se grafica la transformada de Fourier y se guarda
+fourier_DFT=DFT(y_sig)
+plt.figure()
+plt.plot(f_sig, np.abs(fourier_DFT))
+plt.title("Transformada de Fourier de signal")
+plt.xlabel("Frecuencia")
+plt.ylabel("Transformada de Fourier")
+plt.savefig("GarciaCamila_TF.pdf")
